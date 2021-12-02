2c5c671f7285ef2fc78a34fbaf09c218f6e95f29
jph181614 <54452668+jph181614@users.noreply.github.com>
2021-12-02 16:11:36 -0500

    Create Lomuto.h

---
+++ Lomuto.h
@@ -0,0 +1,11 @@
+using namespace std;
+
+class Lomuto {
+    public:
+        Lomuto();
+        void quicksort(int arr[], int p, int r);
+        int lomutoPartition(int arr[], int p, int r);   
+
+    private:
+
+};
