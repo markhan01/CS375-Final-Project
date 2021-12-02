1d3339268023184ee14b1aab6d25e864b191b7ff
John Halleran <jhaller1@cs.binghamton.edu>
2021-12-02 16:58:49 -0500

    file input

--- Driver.cpp
+++ Driver.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <chrono>
+#include "Lomuto.h"
+#include "RandomizedQuicksort.cpp"
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	
+    string input_text, line;
+    input_text = argv[1];
+    ifstream input_file (input_text); 
+    int val;
+    vector<int> vec;
+
+    if (input_file.is_open()) {
+        while (getline(input_file, line)) {
+            stringstream s(line);
+            s >> val;
+            vec.push_back(val);
+        }
+
+
+
+
+
+
+
+
+        vector<int>::iterator it;
+        for (it = vec.begin(); it != vec.end(); it ++)
+            cout << *it << "\n";
+    }
 }
